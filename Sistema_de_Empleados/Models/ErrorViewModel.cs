fc9384d11c97aa3f60bbde3a45664f9814114001
Luis Alejandro Bravo Bello <lb2024-1279@uce.edu.do>
2026-07-04 19:28:16 -0400

    Agregar archivos de proyecto.

---
+++ Sistema_de_Empleados/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_de_Empleados.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
