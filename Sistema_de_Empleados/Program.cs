7c71abc0086061b53ebc806cdfbc9062b24b2356
Luis Alejandro Bravo Bello <lb2024-1279@uce.edu.do>
2026-07-04 20:04:16 -0400

    chore(blazor): add Server-Side Blazor configuration and routing

--- Sistema_de_Empleados/Program.cs
+++ Sistema_de_Empleados/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -24,4 +25,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapBlazorHub();              
+app.MapFallbackToPage("/_Host");
 app.Run();
